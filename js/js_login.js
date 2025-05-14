37c595b98d148fbdca65feb0c2a9d2e7bb25b0da
admin <dkim816@gmail.com>
2025-05-14 10:45:14 +0900

    W11_end_live server_not_working

--- js/js_login.js
+++ js/js_login.js
@@ -8,12 +8,22 @@ function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
         }
     session_check(); // 세션 유무 검사
 }
+
+function init_logined(){
+    if(sessionStorage){
+        decrypt_text(); // 복호화 함수
+    }
+    else{
+        alert("세션 스토리지 지원 x");
+    }
+}
+
 const check_input = () => {
     const loginForm = document.getElementById('login_form');
     const loginBtn = document.getElementById('login_btn');
     const emailInput= document.getElementById('typeEmailX');
     const passwordInput = document.getElementById('typePasswordX');
-    const idsave_check = document.getElementById('idSaveCheck');
+
 
 
     const c = '아이디, 패스워드를체크합니다';
@@ -25,6 +35,13 @@ const check_input = () => {
     // check_xss 함수로 비밀번호 Sanitize
     const sanitizedEmail = check_xss(emailValue);
     // check_xss 함수로 비밀번호 Sanitize
+    const idsave_check = document.getElementById('idSaveCheck');
+
+    const payload = {
+        id: emailValue,
+        exp: Math.floor(Date.now() / 1000) + 3600 // 1시간 (3600초)
+    };
+    const jwtToken = generateJWT(payload);
 
     if (emailValue === '') {
         alert('이메일을입력하세요.');
@@ -76,6 +93,7 @@ const check_input = () => {
     console.log('이메일:', emailValue);
     console.log('비밀번호:', passwordValue);
     session_set(); // 세션 생성
+    localStorage.setItem('jwt_token', jwtToken);
     loginForm.submit();
 };
 function setCookie(name, value, expiredays) {
